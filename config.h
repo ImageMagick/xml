8f7e69a6209a3deb820968fa9ce520fdf12bd1f5
Dirk Lemstra <dirk@lemstra.org>
2025-08-01 11:22:18 +0200

    Created header files with cmake.

---
+++ config.h
@@ -0,0 +1,38 @@
+/* Define to 1 if you have the declaration of 'getentropy', and to 0 if you
+   don't. */
+#define HAVE_DECL_GETENTROPY 0
+
+/* Define to 1 if you have the declaration of 'glob', and to 0 if you don't.
+   */
+#define HAVE_DECL_GLOB 0
+
+/* Define to 1 if you have the declaration of 'mmap', and to 0 if you don't.
+   */
+#define HAVE_DECL_MMAP 0
+
+/* Define if __attribute__((destructor)) is accepted */
+/* #undef HAVE_FUNC_ATTRIBUTE_DESTRUCTOR */
+
+/* Have dlopen based dso */
+/* #undef HAVE_DLOPEN */
+
+/* Define if history library is there (-lhistory) */
+/* #undef HAVE_LIBHISTORY */
+
+/* Define if readline library is there (-lreadline) */
+/* #undef HAVE_LIBREADLINE */
+
+/* Define to 1 if you have the <poll.h> header file. */
+/* #undef HAVE_POLL_H */
+
+/* Have shl_load based dso */
+/* #undef HAVE_SHLLOAD */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* System configuration directory (/etc) */
+#define XML_SYSCONFDIR "C:/Program Files (x86)/libxml2/etc"
+
+/* TLS specifier */
+/* #undef XML_THREAD_LOCAL */
